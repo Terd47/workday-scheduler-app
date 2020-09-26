b0cffaff726fd34f59ae5270af5498f49d191db8
Terd47 <terdlar@gmail.com>
2020-09-26 02:01:27 -0400

    save to storage function fixed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,107 +5,79 @@ function currentDay(){
     var addDateTime = $('#currentDay').text(todayDate);
 }
 currentDay();
+var CurrentTime = moment().format('hA');
+workSchedule = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM','3PM','4PM','5PM'];
 
-
-var nine = 9;
-var ten = 10;
-var eleven = 11;
-var twelve = 12;
-var thirteen = 13;
-var fourteen = 14;
-var fifteen = 15;
-var sixteen = 16;
-var seventeen = 17;
-var CurrentTime = moment().format('h');
-console.log(CurrentTime);
-
-console.log(typeof(+CurrentTime));
-if(+CurrentTime == nine){
-    $('#hour-9').addClass('present');
-} else if(CurrentTime < nine){
-    $('#hour-9').addClass('future');
-}else {
-    $('#hour-9').addClass('past');
-}
-if(+CurrentTime == ten){
-    $('#hour-10').addClass('present');
-} else if(CurrentTime < ten){
-    $('#hour-10').addClass('future');
-}else {
-    $('#hour-10').addClass('past');
-}
-if(+CurrentTime == eleven){
-    $('#hour-11').addClass('present');
-} else if(CurrentTime < eleven){
-    $('#hour-11').addClass('future');
-}else {
-    $('#hour-11').addClass('past');
-}
-if(+CurrentTime == twelve){
-    $('#hour-12').addClass('present');
-} else if(CurrentTime < twelve){
-    $('#hour-12').addClass('future');
-}else {
-    $('#hour-12').addClass('past');
-}
-if(+CurrentTime == thirteen){
-    $('#hour-13').addClass('present');
-} else if(CurrentTime < thirteen){
-    $('#hour-13').addClass('future');
-}else {
-    $('#hour-13').addClass('past');
-}
-if(+CurrentTime == fourteen){
-    $('#hour-14').addClass('present');
-} else if(CurrentTime < fourteen){
-    $('#hour-14').addClass('future');
-}else {
-    $('#hour-14').addClass('past');
-}
-if(+CurrentTime == fifteen){
-    $('#hour-15').addClass('present');
-} else if(CurrentTime < fifteen){
-    $('#hour-15').addClass('future');
-}else {
-    $('#hour-15').addClass('past');
-}
-if(+CurrentTime == sixteen){
-    $('#hour-16').addClass('present');
-} else if(CurrentTime < sixteen){
-    $('#hour-16').addClass('future');
-}else {
-    $('#hour-16').addClass('past');
-}
-if(+CurrentTime == seventeen){
-    $('#hour-17').addClass('present');
-} else if(CurrentTime < seventeen){
-    $('#hour-17').addClass('future');
-}else {
-    $('#hour-17').addClass('past');
+for (var i = 0; i < workSchedule.length; i++){
+    if(CurrentTime == workSchedule[i]){
+        $('.time-block').addClass('present');
+    } else if (CurrentTime < workSchedule[i]){
+        $('.time-block').addClass('future');
+    } else{
+        $('.time-block').addClass('past');
+    }
 }
 
-
+var event1 = "";
+event1 = localStorage.getItem('event9');
+$('#save9').text(event1);
+var event2 = "";
+event2 = localStorage.getItem('event10');
+$('#save10').text(event2);
+var event3 = "";
+event3 = localStorage.getItem('event11');
+$('#save11').text(event3);
+var event4= "";
+event4 = localStorage.getItem('event12');
+$('#save12').text(event4);
+var event5 = "";
+event5= localStorage.getItem('event1');
+$('#save1').text(event5);
+var event6 = "";
+event6 = localStorage.getItem('event2');
+$('#save2').text(event6);
+var event7 = "";
+event7 = localStorage.getItem('event3');
+$('#save3').text(event7);
+var event8 = "";
+event8= localStorage.getItem('event4');
+$('#save4').text(event8);
+var event9 = "";
+event9 = localStorage.getItem('event5');
+$('#save5').text(event9);
 
 function saveToStorage (){
     var textArea = $('.description').val();
-    var  saveBtn = $('.saveBtn');
-    var storage = window.localStorage;
-    console.log(textArea);
+    
+    if(textArea !== ""){
+    var getEvent9 = $('#save9').val();
+    var getEvent10 = $('#save10').val();
+    var getEvent11 = $('#save11').val();
+    var getEvent12 = $('#save12').val();
+    var getEvent1 = $('#save1').val();
+    var getEvent2 = $('#save2').val();
+    var getEvent3 = $('#save3').val();
+    var getEvent4 = $('#save4').val();
+    var getEvent5 = $('#save5').val();
 
-    if(localStorage.getItem("textArea") != null){
-        let workEvent = JSON.parse(localStorage.getItem("textArea"));
-        workEvent.push({name, textArea});
-        localStorage.setItem("textArea",JSON.stringify(workEvent));
-        localStorage.getItem("textArea");
-    } else {
-        let workEvent = [{name, textArea}];
-        localStorage.setItem("textArea", JSON.stringify(workEvent));
-        workEvent.text();
+    localStorage.setItem("event9", getEvent9);
+    localStorage.setItem("event10", getEvent10);
+    localStorage.setItem("event11", getEvent11);
+    localStorage.setItem("event12", getEvent12);
+    localStorage.setItem("event1", getEvent1);
+    localStorage.setItem("event2", getEvent2);
+    localStorage.setItem("event3", getEvent3);
+    localStorage.setItem("event4", getEvent4);
+    localStorage.setItem("event5", getEvent5);
+
     }
+    
+    //console.log(storageItem);
 }
 saveToStorage();
 
 $('.saveBtn').on('click', function(){
     console.log('I am Here');
+    
     saveToStorage();
 })
